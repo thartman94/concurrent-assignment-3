ba496dc278900bb672dab300f93e2dd078889005
Tom Hartman <94thartman@gmail.com>
2022-05-03 00:05:21 -0400

    technically working

--- src/App.java
+++ src/App.java
@@ -1,12 +1,6 @@
 import java.util.List;
-import java.util.Scanner;
-import java.util.Map;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Collections;
 
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
@@ -18,6 +12,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingWorker;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.Callable;
 
 /** ... */
 /**
@@ -29,8 +29,6 @@ import javax.swing.JTextField;
  */
 
 public class App implements ActionListener {
-	private static final int WIDTH = 800;
-	private static final int HEIGHT = WIDTH / 16 / 9;
 
 	private JLabel label = new JLabel("Primes Found: ");
 	private JFrame frame = new JFrame("Prime Finder");
@@ -38,38 +36,73 @@ public class App implements ActionListener {
 	private JButton cancel = new JButton("Cancel");
 	private JTextField start = new JTextField(12);
 	private JTextField end = new JTextField(12);
+	SwingWorker<Object, Integer> Worker;
 
-	private void Calculate(int end, int start) {
-		Scanner sc = new Scanner(System.in);
-		UI ui = new UI(sc);
-		List<Integer> synPrimes = Collections.synchronizedList(new ArrayList<Integer>());
-		Map<Integer, List<Integer>> synNonPrimes = Collections
-				.synchronizedMap(new LinkedHashMap<Integer, List<Integer>>());
+	static class Task implements Callable<List<Integer>> {
+		private int number;
 
-		double startTime = System.currentTimeMillis();
-		new GuiBounded(synPrimes, synNonPrimes, end, 0);
-		double endTime = System.currentTimeMillis();
-		double time = endTime - startTime;
-		ui.printResults("Bounded", end, time, synPrimes, synNonPrimes);
+		public Task(int number) {
+			this.number = number;
+		}
+
+		@Override
+		public List<Integer> call() throws Exception {
+			Factors fc = new Factors();
+			List<Integer> factors = fc.getFactors(number);
+			return factors;
+		}
 	}
 
-	public App() {
-		submit.addActionListener(
-				new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						submit.setEnabled(false);
-						label.setText(start.getText());
-						Calculate(Integer.valueOf(end.getText()), Integer.valueOf(start.getText()));
-					}
-				});
-		cancel.addActionListener(
-				new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						submit.setEnabled(true);
-						label.setText(end.getText());
+	void findPrimes() {
+		Worker = new SwingWorker<Object, Integer>() {
+			int count = 0;
+			ExecutorService service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+			List<Future<List<Integer>>> allFutures = new ArrayList<>();
 
+			@Override
+			protected Object doInBackground() throws Exception {
+				submit.setEnabled(false);
+				for (int i = Integer.valueOf(start.getText()); i <= Integer.valueOf(end.getText()); i++) {
+					if (!isCancelled()) {
+						Future<List<Integer>> future = service.submit(new Task(i));
+						allFutures.add(future);
 					}
-				});
+				}
+
+				for (int j = Integer.valueOf(start.getText()); j <= Integer.valueOf(end.getText()); j++) {
+					if (!isCancelled()) {
+						Future<List<Integer>> future = allFutures.get(j);
+						try {
+							List<Integer> factors = future.get();
+							if (factors.size() == 2) {
+								count++;
+								publish(count);
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				}
+
+				return null;
+			}
+
+			@Override
+			protected void process(List<Integer> chunks) {
+				label.setText("Primes Found: " + chunks.get(chunks.size() - 1));
+			}
+
+			@Override
+			protected void done() {
+				submit.setEnabled(true);
+			}
+		};
+		Worker.execute();
+	};
+
+	public App() {
+		submit.addActionListener(this);
+		cancel.addActionListener(this);
 
 		JPanel panel = new JPanel();
 		panel.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
@@ -90,77 +123,16 @@ public class App implements ActionListener {
 
 	public static void main(String[] args) throws Exception {
 
-		Scanner sc = new Scanner(System.in);
 		new App();
-		UI ui = new UI(sc);
-		int operation = 0;
-		boolean running = true;
 
-		while (running) {
-			operation = ui.getOpperation();
-
-			List<Integer> primes = new ArrayList<Integer>();
-			Map<Integer, List<Integer>> nonPrimes = new LinkedHashMap<Integer, List<Integer>>();
-
-			if (operation == 1) {
-				int input = ui.getInput();
-				double startTime = System.currentTimeMillis();
-
-				for (int i = 1; i <= input; i++) {
-					SingleThread st = new SingleThread(primes, nonPrimes, i);
-					st.run();
-				}
-				double endTime = System.currentTimeMillis();
-				double time = endTime - startTime;
-
-				ui.printResults("Single-threaded", input, time, primes, nonPrimes);
-			}
-
-			else if (operation == 2) {
-				List<Integer> synPrimes = Collections.synchronizedList(primes);
-				Map<Integer, List<Integer>> synNonPrimes = Collections.synchronizedMap(nonPrimes);
-				int input = ui.getInput();
-				double startTime = System.currentTimeMillis();
-
-				for (int i = 1; i <= input; i++) {
-					MultiThread mt = new MultiThread(synPrimes, synNonPrimes, i);
-					mt.start();
-				}
-
-				while (synPrimes.size() + synNonPrimes.size() != input) {
-				}
-
-				double endTime = System.currentTimeMillis();
-				double time = endTime - startTime;
-
-				ui.printResults("Multi-threaded", input, time, synPrimes, synNonPrimes);
-			}
-
-			else if (operation == 3) {
-				List<Integer> synPrimes = Collections.synchronizedList(primes);
-				Map<Integer, List<Integer>> synNonPrimes = Collections.synchronizedMap(nonPrimes);
-
-				int threads = ui.getThreads();
-				int input = ui.getInput();
-
-				double startTime = System.currentTimeMillis();
-				new Bounded(synPrimes, synNonPrimes, input, threads);
-				double endTime = System.currentTimeMillis();
-				double time = endTime - startTime;
-				ui.printResults("Bounded", input, time, synPrimes, synNonPrimes);
-			}
-
-			else {
-				System.out.println("Exiting...");
-				sc.close();
-				running = false;
-			}
-		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
+		if (e.getSource() == submit) {
+			findPrimes();
+		} else if (e.getSource() == cancel) {
+			Worker.cancel(true);
+		}
 	}
 }
