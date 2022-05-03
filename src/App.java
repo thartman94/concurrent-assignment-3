c5df38005018fc46000f734e0778c6f9d82d135a
Tom Hartman <94thartman@gmail.com>
2022-05-02 21:50:23 -0400

    basic gui

--- src/App.java
+++ src/App.java
@@ -5,6 +5,20 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Collections;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 /** ... */
 /**
  * 
@@ -14,10 +28,70 @@ import java.util.Collections;
  * @throws Exception
  */
 
-public class App {
+public class App implements ActionListener {
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = WIDTH / 16 / 9;
+
+	private JLabel label = new JLabel("Primes Found: ");
+	private JFrame frame = new JFrame("Prime Finder");
+	private JButton submit = new JButton("Submit");
+	private JButton cancel = new JButton("Cancel");
+	private JTextField start = new JTextField(12);
+	private JTextField end = new JTextField(12);
+
+	private void Calculate(int end, int start) {
+		Scanner sc = new Scanner(System.in);
+		UI ui = new UI(sc);
+		List<Integer> synPrimes = Collections.synchronizedList(new ArrayList<Integer>());
+		Map<Integer, List<Integer>> synNonPrimes = Collections
+				.synchronizedMap(new LinkedHashMap<Integer, List<Integer>>());
+
+		double startTime = System.currentTimeMillis();
+		new GuiBounded(synPrimes, synNonPrimes, end, 0);
+		double endTime = System.currentTimeMillis();
+		double time = endTime - startTime;
+		ui.printResults("Bounded", end, time, synPrimes, synNonPrimes);
+	}
+
+	public App() {
+		submit.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						submit.setEnabled(false);
+						label.setText(start.getText());
+						Calculate(Integer.valueOf(end.getText()), Integer.valueOf(start.getText()));
+					}
+				});
+		cancel.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						submit.setEnabled(true);
+						label.setText(end.getText());
+
+					}
+				});
+
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
+		panel.setLayout(new GridLayout(0, 2));
+		panel.add(new JLabel("Start:"));
+		panel.add(start);
+		panel.add(new JLabel("End:"));
+		panel.add(end);
+		panel.add(submit);
+		panel.add(cancel);
+		panel.add(label);
+
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
 	public static void main(String[] args) throws Exception {
 
 		Scanner sc = new Scanner(System.in);
+		new App();
 		UI ui = new UI(sc);
 		int operation = 0;
 		boolean running = true;
@@ -28,9 +102,6 @@ public class App {
 			List<Integer> primes = new ArrayList<Integer>();
 			Map<Integer, List<Integer>> nonPrimes = new LinkedHashMap<Integer, List<Integer>>();
 
-			/**
-			 * Single Threaded Execution
-			 */
 			if (operation == 1) {
 				int input = ui.getInput();
 				double startTime = System.currentTimeMillis();
@@ -45,9 +116,6 @@ public class App {
 				ui.printResults("Single-threaded", input, time, primes, nonPrimes);
 			}
 
-			/**
-			 * Unbounded Multi-threaded Execution
-			 */
 			else if (operation == 2) {
 				List<Integer> synPrimes = Collections.synchronizedList(primes);
 				Map<Integer, List<Integer>> synNonPrimes = Collections.synchronizedMap(nonPrimes);
@@ -68,9 +136,6 @@ public class App {
 				ui.printResults("Multi-threaded", input, time, synPrimes, synNonPrimes);
 			}
 
-			/**
-			 * Bounded Multi-threaded Execution
-			 */
 			else if (operation == 3) {
 				List<Integer> synPrimes = Collections.synchronizedList(primes);
 				Map<Integer, List<Integer>> synNonPrimes = Collections.synchronizedMap(nonPrimes);
@@ -79,20 +144,23 @@ public class App {
 				int input = ui.getInput();
 
 				double startTime = System.currentTimeMillis();
-				Bounded bd = new Bounded(synPrimes, synNonPrimes, input, threads);
+				new Bounded(synPrimes, synNonPrimes, input, threads);
 				double endTime = System.currentTimeMillis();
 				double time = endTime - startTime;
 				ui.printResults("Bounded", input, time, synPrimes, synNonPrimes);
 			}
 
-			/**
-			 * Exit
-			 */
 			else {
 				System.out.println("Exiting...");
 				sc.close();
 				running = false;
 			}
 		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 }
---
+++ src/GuiBounded.java
@@ -0,0 +1,76 @@
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * Handles the bounded execution of the program.
+ */
+public class GuiBounded {
+
+	List<Integer> primes = new ArrayList<Integer>();
+	Map<Integer, List<Integer>> nonPrimes = new LinkedHashMap<Integer, List<Integer>>();
+	int input;
+	int threads;
+
+	/**
+	 * Constructor for the Bounded class.
+	 * 
+	 * @param primes    The list of primes.
+	 * @param nonPrimes The map of non-primes.
+	 * @param input     The the value to be factored, provided by user
+	 * @param threads   The number of threads, provided by user
+	 */
+	public GuiBounded(List<Integer> primes, Map<Integer, List<Integer>> nonPrimes, int input, int threads) {
+		this.primes = primes;
+		this.nonPrimes = nonPrimes;
+		this.input = input;
+		this.threads = threads;
+
+		if (threads == 0) {
+			threads = 1 + Runtime.getRuntime().availableProcessors();
+		}
+
+		ExecutorService service = Executors.newFixedThreadPool(10);
+
+		List<Future<List<Integer>>> allFutures = new ArrayList<>();
+		for (int j = 0; j <= input; j++) {
+			Future<List<Integer>> future = service.submit(new Task(j));
+			allFutures.add(future);
+		}
+
+		for (int j = 0; j <= input; j++) {
+			Future<List<Integer>> future = allFutures.get(j);
+			try {
+				List<Integer> factors = future.get();
+				if (factors.size() == 2) {
+					primes.add(j);
+				} else if (factors.size() > 2) {
+					nonPrimes.put(j, factors);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Task implements Callable<List<Integer>> {
+		private int number;
+
+		public Task(int number) {
+			this.number = number;
+		}
+
+		@Override
+		public List<Integer> call() throws Exception {
+			Factors fc = new Factors();
+			List<Integer> factors = fc.getFactors(number);
+			return factors;
+		}
+	}
+}
